15ea78fdce4f427a6cdb8f03deb4884eabd03efc
shreyap19 <129271094+shreyap19@users.noreply.github.com>
2025-12-01 13:47:00 +0530

    Update round_off.v

--- src/round_off.v
+++ src/round_off.v
@@ -1,3 +1,17 @@
-module round(
-)
+module round_off(
+  input wire [63:0]shifted_mantissa,
+  input wire k_out,
+  output reg mantissa_out
+);
+
+reg int no_bt;
   
+  always@()
+  begin
+    if(k_out >= 0)
+      no_bt  = 32-1-(k_out+2)-3;
+    else
+      no_bt  = 32-1-((-k_out)+2)-3;
+  end 
+  
+  assign mantissa_out = { data_in[63:(63-no_bt+1)], 9'b0 };
