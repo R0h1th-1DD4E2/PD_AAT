8dda32109528e5784afd745f67d00154f0cb8e43
selva-kumar-m <mmskrahul@gmail.com>
2025-12-01 16:32:26 +0530

    posit decoder tb updated

--- tb/posit_decoder_tb.v
+++ tb/posit_decoder_tb.v
@@ -2,22 +2,24 @@ module posit_decoder_tb;
 
 
 reg [31:0] posit_num;
-reg start,clk,rst;
-wire  sign, done;
+reg start,clk,rst,received;
+wire  sign, done,ZERO,NAR;
 wire  [5:0] k;//k (regime value) it can take values in the range [-31,30], so 6 bits to represent, k is the run length of regime.
 wire [2:0] exp_value;
 wire [31:0] mantissa;
 
 
-posit_decoder dut1(posit_num,start,clk,rst,sign,done,k,exp_value,mantissa);
+posit_decoder dut1(posit_num,start,clk,rst,received,sign,done,ZERO,NAR,k,exp_value,mantissa);
 
 
 initial begin
 clk=0;
-rst=1;#100;
-posit_num=32'b00001101110011001100110011001100;
-rst=0;
-start=1;
+received=0;
+rst=0;#100;
+posit_num=32'h80000000;
+rst=1;
+start=1;#100;
+received=1;
 
 #1000;
 $finish;
